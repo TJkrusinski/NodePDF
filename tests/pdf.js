d51a82ae3f16fcf3204e9b15f3621c178b7aacad
arve0 <arve.seljebu@gmail.com>
2015-02-26 15:56:27 +0100

    fix tests

--- tests/pdf.js
+++ tests/pdf.js
@@ -4,120 +4,149 @@ var assert = require('assert');
 
 var Pdf = require('../index.js');
 var child = require('../child.js');
+var fs = require('fs');
+var path = require('path');
 var FP = process.env.PWD || process.cwd() || __dirname;
 
-describe('child#supports()', function(){
-  it('checks to see if phantomjs is installed', function(d){
-    child.supports(function(exists){
-      assert.ok(exists);
-      d();
+function exists(filename) {
+  filename = FP + '/' + filename;
+  try {
+    var fd = fs.openSync(filename, 'r');
+  } catch (e) {
+    return false;
+  }
+  fs.close(fd);
+  return true;
+}
+
+// clean up before and after
+describe('all tests', function() {
+  var files = ['httpbin.org.pdf', 'about\:blank.pdf', 'www.yahoo.com.pdf', 'httpbin.org/html.pdf'];
+  function deleteAll(){
+    files.forEach(function(filename){
+      if (!exists(filename)) return;
+      filename = path.join(FP, filename);
+      console.log('deleting ' + filename);
+      fs.unlinkSync(filename);
+    });
+  }
+  before(deleteAll);
+  after(deleteAll);
+
+  describe('child#supports()', function(){
+    it('checks to see if phantomjs is installed', function(d){
+      child.supports(function(exists){
+        assert(exists);
+        d();
+      });
     });
   });
-});
 
-describe('child#supports()', function(){
-  it('checks to see if asdfhijk is installed', function(d){
-    child.supports(function(exists){
-      assert.ok(!exists);
-      d();
-    }, 'asdfhijk');
-  });
-});
-
-// google vs yahoo
-
-describe('pdf#done() 1', function(){
-  it('fires done when ', function(d){
-    this.timeout(20000);
-    var pdf1 = new Pdf('http://www.google.com', 'google.pdf');
-    pdf1.on('done', function(msg){
-      assert.ok(msg);
-      assert.equal(FP + '/google.pdf', msg);
-      d();
-    });
-    pdf1.on('error', function(msg){
-      assert.ok(false);
-      d();
+  describe('child#supports()', function(){
+    it('checks to see if asdfhijk is installed', function(d){
+      child.supports(function(exists){
+        assert(!exists);
+        d();
+      }, 'asdfhijk');
     });
   });
-});
 
-describe('pdf#content', function() {
-  it('fires done when content is loaded', function(d) {
-    var pdf1 = new Pdf(null, 'html.pdf', {
-      'content': '<html><body>Test</body></html>'
-    });
-    pdf1.on('done', function(msg){
-      assert.ok(msg);
-      assert.equal(FP + '/html.pdf', msg);
-      d();
-    });
-    pdf1.on('error', function(msg){
-      assert.ok(false);
-      d();
-    });
-  })
-});
+  // google vs yahoo
 
-describe('pdf#done() 2', function(){
-  it('fires done when ', function(d){
-    this.timeout(20000);
-    var pdf2 = new Pdf('http://yahoo.com', 'yahoo.pdf', {
-      'viewportSize': {
-        'width': 3000,
-        'height': 9000
-      },
-      'paperSize': {
-        'pageFormat': 'A4',
-        'margin': {
-          'top': '2cm'
+  describe('pdf#done() 1', function(){
+    it('fires done when ', function(d){
+      this.timeout(20000);
+      var pdf1 = new Pdf('http://httpbin.org/');
+      pdf1.on('done', function(){
+        assert(exists('httpbin.org.pdf'));
+        d();
+      });
+      pdf1.on('error', function(msg){
+        console.log(msg);
+        assert(false);
+        d();
+      });
+    });
+  });
+
+  describe('pdf#content', function() {
+    it('fires done when content is loaded', function(d) {
+      this.timeout(5000);
+      var pdf1 = new Pdf(null, {
+        'content': '<html><body>Test</body></html>'
+      });
+      pdf1.on('done', function(){
+        assert(exists('about\:blank.pdf'));
+        d();
+      });
+      pdf1.on('error', function(msg){
+        console.log(msg);
+        assert(false);
+        d();
+      });
+    })
+  });
+
+  describe('pdf#done() 2', function(){
+    it('fires done when ', function(d){
+      this.timeout(20000);
+      var pdf2 = new Pdf('http://www.yahoo.com', {
+        'viewportSize': {
+          'width': 3000,
+          'height': 9000
         },
-        'header': {
-          'height': '4cm',
-          'contents': 'HEADER {currentPage} / {pages}'
+        'paperSize': {
+          'pageFormat': 'A4',
+          'margin': {
+            'top': '2cm'
+          },
+          'header': {
+            'height': '4cm',
+            'contents': 'HEADER {currentPage} / {pages}'
+          },
+          'footer': {
+            'height': '4cm',
+            'contents': 'FOOTER {currentPage} / {pages}'
+          }
         },
-        'footer': {
-          'height': '4cm',
-          'contents': 'FOOTER {currentPage} / {pages}'
-        }
-      },
-      'zoomFactor': 1.1,
-      'cookies': [
-        {
-          'name':     'Valid-Cookie-Name 1',   /* required property */
-          'value':    'Valid-Cookie-Value 1',  /* required property */
-          'domain':   'localhost',           /* required property */
-          'path':     '/foo',
-          'httponly': true,
-          'secure':   false,
-          'expires':  (new Date()).getTime() + (1000 * 60 * 60)   /* <-- expires in 1 hour */
-        },
-        {
-          'name':     'Valid-Cookie-Name 2',
-          'value':    'Valid-Cookie-Value 2',
-          'domain':   'localhost'
-        }
-      ]
-    });
-    pdf2.on('done', function(msg){
-      assert.ok(msg);
-      assert.equal(FP + '/yahoo.pdf', msg);
-      d();
-    });
-    pdf2.on('error', function(msg){
-      assert.ok(false);
-      d();
+        'zoomFactor': 1.1,
+        'cookies': [
+          {
+            'name':     'Valid-Cookie-Name 1',   /* required property */
+            'value':    'Valid-Cookie-Value 1',  /* required property */
+            'domain':   'localhost',           /* required property */
+            'path':     '/foo',
+            'httponly': true,
+            'secure':   false,
+            'expires':  (new Date()).getTime() + (1000 * 60 * 60)   /* <-- expires in 1 hour */
+          },
+          {
+            'name':     'Valid-Cookie-Name 2',
+            'value':    'Valid-Cookie-Value 2',
+            'domain':   'localhost'
+          }
+        ]
+      });
+      pdf2.on('done', function(){
+        assert(exists('www.yahoo.com.pdf'));
+        d();
+      });
+      pdf2.on('error', function(msg){
+        assert(false);
+        d();
+      });
     });
   });
-});
 
-describe('pdf#render()', function(){
-  it('renders a pdf with a callback style', function(d){
-    this.timeout(5000);
-    Pdf.render('http://www.google.com', 'google2.pdf', function(err, file){
-      assert.equal(err, null);
-      assert.equal(FP + '/google2.pdf', file);
-      d();
+  describe('pdf#render()', function(){
+    it('renders a pdf with a callback style', function(d){
+      this.timeout(5000);
+      Pdf.render('http://httpbin.org/html', function(err){
+        assert.equal(err, null);
+        assert(exists('httpbin.org/html.pdf'));
+        d();
+      });
     });
   });
+
 });
